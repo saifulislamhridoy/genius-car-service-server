bab360631898bfaca5e830bfec4418abf93a83fb
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-04-24 00:41:36 +0600

    added env

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://geniusUser:rTijCftzUEvv7JgQ@cluster0.ohvaa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ohvaa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
